161eacb98ae10d0ad2ae2f9910457f4d21406d5c
hiro <hirohiro716@gmail.com>
2025-11-03 15:47:24 +0900

    Add user script

---
+++ gmail-remove-spam-button.user.js
@@ -0,0 +1,67 @@
+// ==UserScript==
+// @name         Gmail Remove Spam Button
+// @namespace    https://github.com/hirohiro716/
+// @version      1.0
+// @description  Remove the spam email report button.
+// @author       hiro
+// @match        https://mail.google.com/mail/*
+// @icon         https://mail.google.com/favicon.ico
+// @grant        none
+// @updateURL    https://github.com/hirohiro716/gmail-remove-spam-button/raw/main/gmail-remove-spam-button.user.js
+// @downloadURL  https://github.com/hirohiro716/gmail-remove-spam-button/raw/main/gmail-remove-spam-button.user.js
+// ==/UserScript==
+
+const removeButton1 = () => {
+    const button = Array.from(window.document.querySelectorAll('div')).find(element => element.getAttribute('data-tooltip') === '迷惑メールを報告');
+    if (button) {
+        button.addEventListener('mousedown', () => {
+            button.remove();
+        });
+        button.addEventListener('keydown', () => {
+            button.remove();
+        });
+        button.remove();
+    }
+};
+const removeButton2 = () => {
+    const button = Array.from(window.document.querySelectorAll('span')).find(element => element.textContent.endsWith('をブロック'));
+    if (button) {
+        button.addEventListener('mousedown', () => {
+            button.remove();
+        });
+        button.addEventListener('keydown', () => {
+            button.remove();
+        });
+        button.remove();
+    }
+};
+const removeButton3 = () => {
+    const button = Array.from(window.document.querySelectorAll('span')).find(element => element.textContent === 'スパムとして報告');
+    if (button) {
+        button.addEventListener('mousedown', () => {
+            button.remove();
+        });
+        button.addEventListener('keydown', () => {
+            button.remove();
+        });
+        button.remove();
+    }
+};
+const removeButton4 = () => {
+    const button = Array.from(window.document.querySelectorAll('span')).find(element => element.textContent === 'フィッシングを報告');
+    if (button) {
+        const parent = button.parentElement;
+        const message = button.cloneNode();
+        message.textContent = '[スパム報告は禁止です]';
+        message.style.paddingLeft = '1em';
+        message.style.color = '#ccc';
+        message.style.fontSize = '0.8em';
+        parent.replaceChild(message, button);
+    }
+};
+window.addEventListener('load', () => {
+    setInterval(removeButton1, 100);
+    setInterval(removeButton2, 100);
+    setInterval(removeButton3, 100);
+    setInterval(removeButton4, 100);
+});
